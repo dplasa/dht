56f6ccaf60f4f8a011513dab4d94b9d15e33eb8f
Daniel Plasa <dplasa@gmail.com>
2016-10-10 21:31:20 +0200

    Create dht12.ino

---
+++ examples/dht12/dht12.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <dht.h>
+
+dht12 DHT(0x5c);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("DHT12 TEST PROGRAM"));
+}
+
+void loop()
+{
+  unsigned long b = micros();
+  uint8_t chk = DHT.read();
+  unsigned long e = micros();
+
+  Serial.print(F("\nRead sensor: "));
+  switch (chk)
+  {
+    case dht::OK:
+      Serial.print(F("OK, took "));
+      Serial.print (e - b); Serial.println(F(" usec"));
+      break;
+    case dht::ERROR_CHECKSUM:
+      Serial.println(F("Checksum error"));
+      break;
+    case dht::ERROR_TIMEOUT:
+      Serial.println(F("Timeout error"));
+      break;
+    case dht::ERROR_CONNECT:
+      Serial.println(F("Connect error"));
+      break;
+    case dht::ERROR_ACK_L:
+      Serial.println(F("AckL error"));
+      break;
+    case dht::ERROR_ACK_H:
+      Serial.println(F("AckH error"));
+      break;
+    default:
+      Serial.println(F("Unknown error"));
+      break;
+  }
+
+  Serial.print(F("Humidity (%): "));
+  Serial.println(DHT.getHumidity());
+
+  Serial.print(F("Temperature (°C): "));
+  Serial.println(DHT.getTemperature());
+
+  Serial.print(F("Dew Point (°C): "));
+  Serial.println(DHT.dewPoint());
+
+  delay(1000);
+}
+//
+// END OF FILE
+//
