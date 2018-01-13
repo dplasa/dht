1444a1735ba68644d1c39284088b62feb20d31cf
Daniel Plasa <dplasa@gmail.com>
2018-01-13 18:46:08 +0100

    added some cool ASCII art from Seth from arduino.cc

--- examples/dht12/dht12.ino
+++ examples/dht12/dht12.ino
@@ -1,6 +1,29 @@
-//
-// Example for a DHT12 sensor
-//
+ /*  
+ *   Example for a DHT12 sensor
+ *   Beware some of the diagrams about the pinouts are incorrect.
+ *   Pin Definition 1.VDD 2.SDA 3.GND 4.SCL
+ *   Look at the sensor one side has holes to allow air inside. 
+ *   Let's call the surface with holes the FRONT.
+ *   The BACK of the sensor has printed codes and no holes.
+ *   The 4 PINS for connection come out of the BOTTOM of the sensor.
+ *   OK so we have defined the orientation of the sensor.
+ *   Looking at the FRONT of the Sensor with the legs pointing down 
+ *   the LEFTMOST PIN is PIN ONE
+ *   
+ *   
+ *   -----------------
+ *   !   O O O O O   !
+ *   !   O O O O O   !
+ *   !   O O O O O   !
+ *   !   O O O O O   !
+ *   -----------------
+ *       !  !  !  !
+ *       !  !  !  !
+ *       !  !  !  !
+ *       !  !  !  ! 
+ *       1  2  3  4
+ *       
+*/
 
 #include <Wire.h>
 #include <dht.h>
@@ -19,12 +42,12 @@ void loop()
   dht::ReadStatus chk = DHT.read();
   unsigned long e = micros();
 
-  Serial.print(F("\nRead sensor: "));
+  Serial.print(F("Read sensor: "));
   switch (chk)
   {
     case dht::OK:
       Serial.print(F("OK, took "));
-      Serial.print (e - b); Serial.println(F(" usec"));
+      Serial.print (e - b); Serial.print(F(" usec, "));
       break;
     case dht::ERROR_CHECKSUM:
       Serial.println(F("Checksum error"));
@@ -46,16 +69,17 @@ void loop()
       break;
   }
 
-  Serial.print(F("Humidity (%): "));
-  Serial.println(DHT.getHumidity());
+  Serial.print(F("Humidity: "));
+  Serial.print((float)DHT.getHumidity()/(float)10);
+  Serial.print(F("%, "));
 
-  Serial.print(F("Temperature (°C): "));
-  Serial.println(DHT.getTemperature());
+  Serial.print(F(". Temperature (degrees C): "));
+  Serial.print((float)DHT.getTemperature()/(float)10);
 
-  Serial.print(F("Dew Point (°C): "));
+  Serial.print(F(", Dew Point (degrees C): "));
   Serial.println(DHT.dewPoint());
 
-  delay(1000);
+  delay(4000);
 }
 //
 // END OF FILE
